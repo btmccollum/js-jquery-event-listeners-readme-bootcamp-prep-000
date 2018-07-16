c899d9e952409110f5c15344809be8064dec4809
Brad McCollum <btmcco8@gmail.com>
2018-07-16 20:22:48 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -18,7 +18,7 @@ function pressIt() {
 }
 function submitIt() {
   $('form').on('submit', function() {
-    
+    window.alert('Your form is going to be submitted now.');
   });
 }
 $(document).ready(function(){
