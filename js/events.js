625e0d29b18fcb171d0d1b38f736f31404e94d59
Brad McCollum <btmcco8@gmail.com>
2018-07-16 19:20:21 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,14 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function(){
+    alert('Hey!');
+  });
+}
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').append('#tasty')
+  })
+}
 
 $(document).ready(function(){
 
