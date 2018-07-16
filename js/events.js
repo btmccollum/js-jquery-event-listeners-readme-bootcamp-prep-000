5e5dcbbca9412dd963f227f4f154436b6d722685
Brad McCollum <btmcco8@gmail.com>
2018-07-16 20:07:20 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -11,7 +11,7 @@ function frameIt() {
 }
 function pressIt() {
   $('form').on('keydown', function(e) {
-    if (keyCode =  ) {
+    if (keyCode === 71 ) {
       alert('You\'ve pressed the \'G\' key!');
     }
   });
